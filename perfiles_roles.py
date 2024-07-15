2f87f823238de4dc6992a40e9208cc2974c5c806
luislegu1997 <luislegu@hotmail.com>
2024-07-15 11:54:09 -0600

    + rutina perfiles_roles

---
+++ perfiles_roles.py
@@ -0,0 +1,64 @@
+import sys
+import psycopg2
+
+
+
+#loc celaya
+conn = psycopg2.connect(
+    host="127.0.0.1",
+    port=5432,
+    database="imcelaya_loc",
+    user="postgres",
+    password="admin"
+)
+
+
+cursor = conn.cursor()
+
+
+def perfiles_roles(file_path):
+
+    with open(file_path, 'r+') as file:
+
+        for oneline in file:
+
+            lines = oneline.split("N                                                                                                                                            N")
+
+            for line in lines:
+
+                print("----------------")
+
+                line = line.strip()
+
+                line = line.split()
+
+                if len(line) > 1:
+
+                    perfil = line[0]
+
+                    print(f"Perfil: {perfil}")
+                
+                    roles = line[2].split(',')
+
+                    for rol in roles:
+
+                        qry = "SELECT * FROM keplersc.perfiles_roles where perfil=%s and rol=%s " 
+                        cursor.execute(qry, (perfil,rol))
+                        res = cursor.fetchall()
+
+                        if len(res) == 0:
+
+                            print(f"rol: {rol}")
+                             
+                            insert = "insert into keplersc.perfiles_roles(perfil,rol) values(%s,%s)" 
+                            cursor.execute(insert, (perfil,rol ))
+                            conn.commit()
+
+
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    perfiles_roles(str(file_path))
+
+
+
